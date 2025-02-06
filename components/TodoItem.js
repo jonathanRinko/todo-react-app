39e4ea1de2ee6cfc6ccf40b27ee090b808d0718f
unknown <jon.j.rinko@gmail.com>
2025-02-06 11:20:53 -0500

    Updated TodoItem component to use TouchableOpacity

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text, CheckBox, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Button } from "react-native";
+import { CheckBox } from "react-native-elements";
 import styles from './styles'; 
 
 export default function TodoItem({ task, deleteTask, toggleCompleted }) {
@@ -9,18 +10,22 @@ export default function TodoItem({ task, deleteTask, toggleCompleted }) {
                 value={task.completed}
                 onValueChange={() => toggleCompleted(task.id)}
             />
+            <Text style={[styles.todo-itemText, task.completed && styles.completed]}>
+              {task.text}
+           </Text>
+
             {/* <Text style={{ textDecorationLine: task.completed ? 'line-through' : 'none' }}>
-            </Text>
-            <Button title="X" onPress={() => deleteTask(task.id)} /> */}
-            <Text style={[styles.todo-item-text, task.completed && styles.completed]}>
-        {task.text}
-      </Text>
-      <TouchableOpacity
-        style={styles.delete-button}
-        onPress={() => deleteTask(task.id)}
-      >
-        <Text style={{ color: '#fff' }}>Delete</Text>
-      </TouchableOpacity>
+              {task.text}
+            </Text> */}
+
+            {/* <Button title="X" onPress={() => deleteTask(task.id)} /> */}
+              
+          <TouchableOpacity
+            style={styles.deleteButton}
+            onPress={() => deleteTask(task.id)}>
+
+            <Text style={{ color: '#fff' }}>Delete</Text>
+          </TouchableOpacity>
         </View>
     );
 }
